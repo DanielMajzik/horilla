a2425d530fe3a4af6b535b3390cbda40cd772158
Daniel Majzik <majzikbe@gmail.com>
2025-05-07 21:18:46 +0200

    added 'dynamic_fields'

--- horilla/settings.py
+++ horilla/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'dynamic_fields',
     "notifications",
     "mathfilters",
     "corsheaders",
